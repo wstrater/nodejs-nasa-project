0244680a33181663c7433c446f3d89e4a02e8c9b
W Strater <wstrater@yahoo.com>
2024-01-30 14:00:21 -0500

    Update MongoDB configuration for compatibility with testing.

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -5,28 +5,28 @@ require('dotenv').config();
 const { loadLaunchData } = require('../models/launches.model.js');
 const { loadPlanetData } = require('../models/planets.model.js');
 
-const MONGO_URL = process.env.MONGO_URL || 'learnmongodb.whe2o7y.mongodb.net/nasa';
+const MONGO_CONNECTION = process.env.MONGO_CONNECTION || 'learnmongodb.whe2o7y.mongodb.net/nasa?retryWrites=true&w=majority';
 const MONGO_USER = process.env.MONGO_USER || 'nasa-api';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
-const mongoUri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_URL}?retryWrites=true&w=majority`;
+const MONGO_URL = process.env.MONGO_URL || `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CONNECTION}`;
 
 async function finalizeMongo() {
     mongoose.disconnect();
 }
 
 async function initializeMongo() {
-    console.log('initializeMongo', `${MONGO_USER}@${MONGO_URL}`);
+    console.log('initializeMongo', `${MONGO_USER}@${MONGO_CONNECTION}`);
 
     mongoose.connection.once('open', () => {
-        console.log(`MongoDB Connected: ${MONGO_USER}@${MONGO_URL}`);
+        console.log(`MongoDB Connected: ${MONGO_USER}@${MONGO_CONNECTION}`);
     });
 
     mongoose.connection.on('error', err => {
         console.error(err);
     })
 
-    await mongoose.connect(mongoUri);
+    await mongoose.connect(MONGO_URL);
 
     await loadPlanetData();
     await loadLaunchData();
